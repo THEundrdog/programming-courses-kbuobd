39f7b7dd07c756d58a8a2911270a46f8b72571b8
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-20 01:52:42 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,4 +3,10 @@
 //###########################   Programming Courses  ###################
 //######################################################################
 
-console.log("dokha"); 
+
+ let num1: number;
+let num2 : number;
+num1 = 123;
+num2 = 457;
+
+console.log(num1);
