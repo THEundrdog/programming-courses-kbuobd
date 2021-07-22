c81cf81ba19810c1458ce079f67906ec9dfaa894
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-21 13:07:05 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@
 
 
  
-let mynum : number;
-mynum = 100;
-console.log (mynum);  
+let mynumber1, mynumber2, number;
+mynumber1 = 150;
+mynumber2 = 50;
+console.log ("first vairiable content", mynumber1 ); 
+console.log ("second vairiablr content", mynumber2);
+
