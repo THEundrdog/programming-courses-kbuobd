475f3208750d549760e95ff4a7a2e568d297fbc6
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-23 01:54:28 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,22 +1,13 @@
-//######################################################################
+ ;//######################################################################
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
 
 
  
-let mynumber1, mynumber2, number;
-let mynumber3 : number; 
+let mynumber1, mynumber2, number; 
 mynumber1 = 150;
-mynumber2 = 50; 
-mynumber3 = mynumber1;
-mynumber1 = mynumber2; 
-mynumber2 = mynumber3; 
+mynumber2 = 50;
 
-
-
-
-
-console.log ("first vairiable content", mynumber1 ); 
-console.log ("second vairiablr content", mynumber2);
-
+console.log("first vairiable content" , mynumber1 );
+console.log("seconde vairiable content", mynumber2 ); 
