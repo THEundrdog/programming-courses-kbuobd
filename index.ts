1de8188e7245040ffad66d44cb6233bc54d07089
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-23 14:25:53 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -11,4 +11,4 @@ let mynumber1, mynumber2, mynumber3 : number;
 mynumber1 = parseInt(prompt("مرجو كتابة ادخال العدد لاول"));
 mynumber2 = parseInt(prompt("المرجو كتابة ادخال العدد الثاني"));
 mynumber3 = mynumber1 + mynumber2;
-console.log("mynumber3");  
+console.log("mynumber3");   
