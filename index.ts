46c210d175e316df45b0413b68753aa6d4e70818
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-23 14:23:12 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,21 +1,14 @@
  ;//######################################################################
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
-//######################################################################
+//################################################################
 
 
- 
-let mynumber1, mynumber2, number; 
-mynumber1 = 150;
-mynumber2 = 50;
- 
-let mynumber3 : number; 
-mynumber3 = mynumber1; 
-mynumber1 = mynumber2; 
-mynumber2 = mynumber3; 
- 
 
 
-console.log("first vairiable content" , mynumber1 );
-console.log("seconde vairiable content", mynumber2 ); 
- 
+let mynumber1, mynumber2, mynumber3 : number;
+
+mynumber1 = parseInt(prompt("مرجو كتابة العدد لاول"));
+mynumber2 = parseInt(prompt("المرجو كتابة العدد الثاني"));
+mynumber3 = mynumber1 + mynumber2;
+console.log("mynumber3"); 
