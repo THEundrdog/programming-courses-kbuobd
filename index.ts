4f4327bfb0ac7655a969bf776fb5e652a780c680
THEundrdog <chakibtheunderdog@gmail.com>
2021-07-23 13:44:34 -1200

    1 file modified

--- index.ts
+++ index.ts
@@ -8,6 +8,14 @@
 let mynumber1, mynumber2, number; 
 mynumber1 = 150;
 mynumber2 = 50;
+ 
+let mynumber3 : number; 
+mynumber3 = mynumber1; 
+mynumber1 = mynumber2; 
+mynumber2 = mynumber3; 
+ 
+
 
 console.log("first vairiable content" , mynumber1 );
 console.log("seconde vairiable content", mynumber2 ); 
+ 
